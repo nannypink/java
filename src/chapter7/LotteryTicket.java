aa90b5d620e598464ee654f86f074841c5fc3a53
Tina <tina.r.brien@gmail.com>
2023-05-30 14:18:03 +0100

    Chapter 7a

---
+++ src/chapter7/LotteryTicket.java
@@ -0,0 +1,15 @@
+package chapter7;
+
+public class LotteryTicket {
+    private static final int LENGTH = 6;
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] generateNumbers() {
+        int[] ticket = new int[LENGTH];
+
+        Random random = new Randon();
+    }
+}
